dad1d6760643ad270dd9ea78f8aa6a8ebbacca3e
Amruta Dhondage <amrutadh@gmail.com>
2017-04-19 21:14:12 -0700

    async sample 2 with function printing

---
+++ Test_NodeJS/asynCSample2.js
@@ -0,0 +1,37 @@
+var http = require('http');
+
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello World\n");
+});
+
+// Listen on port 8000, IP defaults to 127.0.0.1
+server.listen(8000);
+
+
+// Put a friendly message on the terminal
+console.log("Server running at http://127.0.0.1:8000/");
+
+var async = require("async");
+
+async.series({
+	one : function(c){
+		console.log("1");
+		c(null,"hello");
+	},
+	two : function(c){
+		console.log("2");
+		c(null,"there");
+		
+	},
+	three : function(c){
+		console.log("3");
+		c(null,"class!");
+		
+	}
+},
+	function(err, results) {
+		console.log(results);
+	}
+);
